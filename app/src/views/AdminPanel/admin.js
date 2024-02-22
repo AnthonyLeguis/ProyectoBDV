6f9d68acc5b0decb973c870bb9ca2bee107ef127
Anthony Leguisamo <anthony.leguisamo.g@outlook.com>
2024-02-22 07:52:11 -0400

    grid dashboard

--- app/src/views/AdminPanel/admin.js
+++ app/src/views/AdminPanel/admin.js
@@ -5,3 +5,29 @@ hamburguer.addEventListener('click', () => {
     sidebar.classList.toggle("expand");
 
 });
+
+function isLargeScreen() {
+    return window.innerWidth >= 950;
+  }
+  
+  function expandSidebar() {
+    sidebar.classList.add("expand");
+  }
+  
+  function collapseSidebar() {
+    sidebar.classList.remove("expand");
+  }
+  
+  window.addEventListener('load', () => {
+    if (isLargeScreen()) {
+      expandSidebar();
+    }
+  });
+  
+  window.addEventListener('resize', () => {
+    if (isLargeScreen()) {
+      expandSidebar();
+    } else {
+      collapseSidebar();
+    }
+  });
