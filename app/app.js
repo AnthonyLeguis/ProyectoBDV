c1a48b607e7255a0ff3cd0f00f5b0744e5864670
Anthony Leguisamo <anthony.leguisamo.g@outlook.com>
2024-02-27 18:19:37 -0400

    conexion a la BD

--- app/app.js
+++ app/app.js
@@ -1,20 +1,41 @@
-const express = require('express')
-const path = require('path')
-const expressLayouts = require('express-ejs-layouts')
-const router = require('./routes/router')
+const express = require('express');
+const path = require('path');
+const expressLayouts = require('express-ejs-layouts');
+const router = require('./routes/router');
 
-const app = express()
+const app = express();
 
-app.set('view engine', 'ejs')
-app.use(expressLayouts)
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
 
-app.use(express.static(path.join(__dirname, 'public')))
+const dotenv = require('dotenv');
+dotenv.config({path: './env/.env'});
+
+
+// directorio public
+app.use('/resources', express.static(path.join(__dirname, '/public')));
+
+//Motor de plantilla
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+
+//bcrypt para las contrasenas
+const bcrypt = require('bcryptjs');
+
+//Variables de sessions
+const session = require('express-session')
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+// Modulo de conexion a la bd
+const connection = require('./database/db');
 
 //Uso del router
-app.use(router.routes)
+app.use(router.routes);
 
-
-
-app.listen(3000, ()=> {
+app.listen(3000, (req, res)=> {
     console.log('Servidor corriendo en el puerto http://localhost:3000')
-})
+});
